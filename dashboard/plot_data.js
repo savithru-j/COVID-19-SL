9a1b59a862a25e036403ea6bce8ba87ff1b8e701
Savithru Jayasinghe <43912545+savithru-j@users.noreply.github.com>
2020-03-23 23:59:07 -0400

    Added prediction function and a new data series to show prediction trajectory

--- dashboard/plot_data.js
+++ dashboard/plot_data.js
@@ -5,7 +5,7 @@ var date_format = 'DD-MM-YYYY';
 
 function getDataSriLanka()
 {
-  var data = [];
+  let data = [];
   data.push({t: moment('11-03-2020', date_format), y: 1});
   data.push({t: moment('12-03-2020', date_format), y: 2});
   data.push({t: moment('13-03-2020', date_format), y: 3});
@@ -18,12 +18,13 @@ function getDataSriLanka()
   data.push({t: moment('20-03-2020', date_format), y: 66});
   data.push({t: moment('21-03-2020', date_format), y: 72});
   data.push({t: moment('22-03-2020', date_format), y: 78});
+  data.push({t: moment('23-03-2020', date_format), y: 87});
   return data;
 }
 
 function getDataItaly()
 {
-  var data = [];
+  let data = [];
   data.push({t: moment('31-01-2020', date_format), y: 2});
   data.push({t: moment('06-02-2020', date_format), y: 3});
   data.push({t: moment('21-02-2020', date_format), y: 20});
@@ -56,6 +57,7 @@ function getDataItaly()
   data.push({t: moment('19-03-2020', date_format), y: 41035});
   data.push({t: moment('20-03-2020', date_format), y: 47021});
   data.push({t: moment('21-03-2020', date_format), y: 53578});
+  data.push({t: moment('22-03-2020', date_format), y: 59138});
   return data;
 }
 
@@ -70,20 +72,22 @@ window.onload = function()
 
 function updateChart()
 {
-  var canvas = document.getElementById('chart_canvas');
+  let canvas = document.getElementById('chart_canvas');
   canvas.width = 0.7*window.innerWidth;
   canvas.height = 0.65*window.innerHeight;
-  var ctx = canvas.getContext('2d');
+  let ctx = canvas.getContext('2d');
     
-  var div_controls = document.getElementById('chart_controls');
+  let div_controls = document.getElementById('chart_controls');
   div_controls.style.width = (0.15*window.innerWidth) + "px";
   
-  var check_logy = document.getElementById('check_log_y');
+  let check_logy = document.getElementById('check_log_y');
  
-  var dataSL = getDataSriLanka();
-  var dataIT = getDataItaly();
+  let dataSL = getDataSriLanka();
+  let dataIT = getDataItaly();
   
-  var xaxis_config = {
+  let dataSLpredicted = getPredictionData(dataSL[0].t, 20);
+  
+  let xaxis_config = {
 	    type: 'time',
 			distribution: 'linear',
 			time: {
@@ -101,7 +105,7 @@ function updateChart()
 			}
 		};
 		
-  var yaxis_config = {
+  let yaxis_config = {
 			gridLines: {
 				drawBorder: false
 			},
@@ -125,18 +129,30 @@ function updateChart()
 			data: {
 				datasets: [{
 					label: 'Sri Lanka - confirmed cases',
+					backgroundColor: 'rgba(1,1,1,0)',
 					borderColor: '#66b3ff',
 					data: dataSL,
 					type: 'line',
-					fill: false,
+					fill: true,
+					borderWidth: 2
+				},
+				{
+					label: 'Sri Lanka - predicted',
+					backgroundColor: 'rgba(1,1,1,0)',
+					borderColor: '#66b3ff',
+					borderDash: [5, 5],
+					data: dataSLpredicted,
+					type: 'line',
+					fill: true,
 					borderWidth: 2
 				},
 				{
 					label: 'Italy - confirmed cases',
+					backgroundColor: 'rgba(1,1,1,0)',
 					borderColor: '#fcaf3e',
 					data: dataIT,
 					type: 'line',
-					fill: false,
+					fill: true,
 					borderWidth: 2,
 					hidden: true
 				}]
@@ -148,6 +164,10 @@ function updateChart()
 					xAxes: [xaxis_config],
 					yAxes: [yaxis_config]
 				},
+				legend: {
+            display: true,
+            boxWidth: 10,
+        },
 				plugins: {
 	        zoom: {
 		        // Container for pan options
@@ -211,7 +231,7 @@ function setLogYAxis(is_log)
 
 function setDataOverlayItaly(overlay)  
 {
-  chart_config.data.datasets[1].hidden = !overlay;
+  chart_config.data.datasets[2].hidden = !overlay;
   chart.update();
 }
 
@@ -219,12 +239,12 @@ function alignTimelines(align)
 {
   if (align)
   {
-    for (let datapair of chart_config.data.datasets[1].data)
+    for (let datapair of chart_config.data.datasets[2].data)
       datapair.t.add(41,'days'); //Add offset to Italy dataset
   }
   else //load original datasets
   {
-    chart_config.data.datasets[1].data = getDataItaly();
+    chart_config.data.datasets[2].data = getDataItaly();
   }
   chart.update();
 }
@@ -234,16 +254,93 @@ function resetZoom()
   chart.resetZoom();
 }
 
-function getModelPrediction(days)
+function getPredictionData(start_date, days)
 {
-  let IncubPeriod = 5;
-  let DurMild     = 6;
-  let DurSevere   = 4;
-  let DurICU      = 10;
+  let data = [];
+  let sol_history = predictModel(days);
   
-  let prob_I1_E = 1;
-  let prob_R_I1 = 0.81*prob_I1_E;
+  for (let i = 0; i < sol_history.length; i++)
+  {
+    let num_cases = 0;
+    for (let j = 1; j < 7; ++j)
+      num_cases += sol_history[i][j];
+      
+    data.push({t: start_date.clone().add(i,'days'), y: Math.round(num_cases)});
+  }
   
+  return data;
+}
 
+function predictModel(days)
+{
+  //Periods [days]
+  let T_incub  = 5;
+  let T_mild   = 6;
+  let T_severe = 4;
+  let T_icu    = 10;
+  
+  //Probabilities
+  let prob_I1_E   = 1;  //exposed to mildly infected
+  let prob_R_I1   = 0.81*prob_I1_E; //mild to recovered
+  let prob_I2_I1  = 1 - prob_R_I1; //mild to severe
+  let prob_R_I2   = 0.14/prob_I2_I1; //severe to recovered
+  let prob_I3_I2  = 1 - prob_R_I2; //severe to critical
+  let prob_D_I3   = 0.02/(prob_I3_I2*prob_I2_I1); //critical to dead
+  let prob_R_I3   = 1 - prob_D_I3; //critical to recovered
+  
+  //Rates [1/day]
+  let a   = (1/T_incub)  *prob_I1_E;
+  let g1  = (1/T_mild)   * prob_R_I1;
+  let p1  = (1/T_mild)   * prob_I2_I1;
+  let g2  = (1/T_severe) * prob_R_I2;
+  let p2  = (1/T_severe) * prob_I3_I2;
+  let g3  = (1/T_icu)    * prob_R_I3;
+  let u   = (1/T_icu)    * prob_D_I3;
 
+  //Transmission rates: beta values are always scaled by the population N
+  let b1N = 0.5; //rate at which mildly infected individuals transmit to susceptible people
+  let b2N = 0;   //rate at which severely infected individuals transmit to susceptible people
+  let b3N = 0;   //rate at which critically infected individuals transmit to susceptible people
+  
+  let N = 21.4e6; //Population of Sri Lanka
+  
+  //Normalized transmission rates
+  let b1 = b1N/N;
+  let b2 = b2N/N;
+  let b3 = b3N/N;
+  
+  //Initial solution
+  let E0 = 20;
+  let S0 = N - E0;
+  let I1_0 = 0;
+  let I2_0 = 0;
+  let I3_0 = 0;
+  let R0 = 0;
+  let D0 = 0;
+  
+  //Solution vector: [S, E, I1, I2, I3, R, D] 
+  let solution_hist = [[S0, E0, I1_0, I2_0, I3_0, R0, D0]];
+  
+  for (let i = 0; i < days; i++) 
+  {
+    let sol = solution_hist[i];
+    
+    let dS = -(b1*sol[2] + b2*sol[3] + b3*sol[4])*sol[0]; 
+    let dsol = [dS,                                //dS
+                -dS - a*sol[1],                    //dE
+                a*sol[1] - (g1 + p1)*sol[2],       //dI1
+                p1*sol[2] - (g2 + p2)*sol[3],      //dI2
+                p2*sol[3] - (g3 + u)*sol[4],       //dI3
+                g1*sol[2] + g2*sol[3] + g3*sol[4], //dR
+                u*sol[4]                           //dD
+               ];
+    
+    let new_sol = new Array(7);
+    for (let j = 0; j < 7; j++)
+      new_sol[j] = sol[j] + dsol[j];
+
+    solution_hist.push(new_sol);
+  }
+  
+  return solution_hist;
 }
