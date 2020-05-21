80191ccfcda1e6958771bb7d56cd217994d520c5
Savithru Jayasinghe <43912545+savithru-j@users.noreply.github.com>
2020-05-21 00:02:00 -0400

    Implemented forward model in C++

---
+++ C++/src/CovidSim.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <chrono>
+
+#include "Population.h"
+
+std::vector<Population> predictModel(const ModelParams& params, const Population& pop_init);
+
+int main()
+{
+
+  ModelParams params(75, 14);
+
+  Population pop0(21323733, 5, 1);
+
+  auto t0 = std::chrono::high_resolution_clock::now();
+
+  std::vector<Population> pop_hist;
+  for (int i = 0; i < 1e5; i++)
+    pop_hist = predictModel(params, pop0);
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count()/1000.0 << "s" << std::endl;
+
+  std::cout << pop_hist.size() << std::endl;
+	std::cout << pop_hist.back() << std::endl;
+
+	return 0;
+}
+
+
+std::vector<Population> predictModel(const ModelParams& params, const Population& pop_init)
+{
+  std::vector<Population> population_hist;
+
+  const int nt = params.nt_hist + params.nt_pred;
+  const int nt_sub = 1.0/params.dt;
+
+  Population pop = pop_init;
+
+  population_hist.reserve(nt);
+  population_hist.push_back(pop);
+
+  for (int t = 0; t < nt-1; t++)
+  {
+    for (int j = 0; j < nt_sub; j++) //sub-timestepping [hrs]
+      pop.evolve(params, t);
+
+    pop.report(params, t); //report once daily
+
+    population_hist.push_back(pop); //save solution daily
+  }
+
+  return population_hist;
+}
---
+++ C++/src/ModelParams.h
@@ -0,0 +1,50 @@
+#include <vector>
+
+
+struct ModelParams
+{
+  ModelParams(int nt_hist_, int nt_pred_, double betaN_val = 0.3,
+              double ce_val = 0.0, double c0_val = 0.0,
+              double c1_val = 0.1, double c2_val = 1.0, double c3_val = 1.0)
+    : nt_hist(nt_hist_), nt_pred(nt_pred_)
+  {
+    int nt = nt_hist + nt_pred;
+    betaN.resize(nt);
+    ce.resize(nt);
+    c0.resize(nt);
+    c1.resize(nt);
+    c2.resize(nt);
+    c3.resize(nt);
+
+    for (int i = 0; i < nt; ++i)
+    {
+      betaN[i] = betaN_val;
+      ce[i]    = ce_val;
+      ce[i]    = ce_val;
+      c0[i]    = c0_val;
+      c1[i]    = c1_val;
+      c2[i]    = c2_val;
+      c3[i]    = c3_val;
+    }
+  }
+
+  int nt_hist, nt_pred;
+  const double dt = 1.0/24.0;
+  std::vector<double> betaN;
+  std::vector<double> ce;
+  std::vector<double> c0;
+  std::vector<double> c1;
+  std::vector<double> c2;
+  std::vector<double> c3;
+  double T_incub0 = 3.0;
+  double T_incub1 = 2.0;
+  double T_asympt = 6.0;
+  double T_mild   = 6.0;
+  double T_severe = 4.0;
+  double T_icu    = 10.0;
+  double f        = 0.3;          //exposed to asymptomatic probability
+  double frac_recover_I1 = 0.80;  //fraction of cases that recover from mild-infected stage I1
+  double frac_recover_I2 = 0.75;  //fraction of cases that recover from severe-infected stage I2
+  double CFR      = 0.02;         //case fatality rate
+
+};
