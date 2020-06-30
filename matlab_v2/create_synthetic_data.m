69513ee130a8a97d08234e354a4fb8db51591ac1
Savithru Jayasinghe <43912545+savithru-j@users.noreply.github.com>
2020-06-29 23:43:41 -0400

    New synthetic data

--- matlab_v2/create_synthetic_data.m
+++ matlab_v2/create_synthetic_data.m
@@ -12,19 +12,16 @@ param_vecs = [];
 
 Npop = 1e7; %population
 
-for i = 1:1
-   betaN = getrandomcurve(nt, 0, 1);
+for i = 1:10
+   betaN = getrandomcurve(nt, 0.05, 0.95);
    c0 = zeros(size(betaN));
-   c1 = zeros(size(betaN));
+   c1 = getrandomcurve(nt, 0.05, 0.95);
    c2 = ones(size(betaN));
-%    c0 = getrandomcurve(nt, 1, 1);
-%    c1 = getrandomcurve(nt, 1, 1);
-%    c2 = getrandomcurve(nt, 1, 1);
-   c3 = c2;%getrandomcurve(nt, 0, 1);
+   c3 = c2;
    vec = [betaN; c0; c1; c2; c3; last_params];
    param_vecs = [param_vecs, vec];
    
-   writematrix(vec, sprintf('csv_data/synthetic_SIR%d_params.txt',i),'Delimiter',' ')
+   writematrix(vec, sprintf('csv_data/synthetic%d_params.txt',i),'Delimiter',' ')
 end
 
 plot(param_vecs)
@@ -38,7 +35,7 @@ nc = round(0.05*nt);
 while (true)
     mean = ymin + (ymax-ymin)*rand();
     c = zeros(nt,1);
-    ind = randi(round(0.1*nt), nc, 1);
+    ind = randi(round(0.10*nt), nc, 1);
     c(ind,:) = rand(nc,1);
     c(1,:) = mean*sqrt(nt);
     y = idct(c);
