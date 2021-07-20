356c2a4cfe74e1d3f3a4f558a2b4ef0934c3eb1e
Savithru Jayasinghe <43912545+savithru-j@users.noreply.github.com>
2021-07-19 23:29:32 -0400

    Updated data

--- update_vaccine_data.py
+++ update_vaccine_data.py
@@ -13,7 +13,7 @@ wget.download(url, csv_file_name)
 csvfile = open(csv_file_name, 'r')
 jsonfile = open('world_vaccine_data.js', 'w')
 
-fieldnames = ("Date","Covishield 1","Covishield 2","Sinopharm 1","Sinopharm 2","Sputnik 1","Sputnik 2","Pfizer 1","Pfizer 2")
+fieldnames = ("Date","Covishield 1","Covishield 2","Sinopharm 1","Sinopharm 2","Sputnik 1","Sputnik 2","Pfizer 1","Pfizer 2","Moderna 1","Moderna 2")
 csvreader = csv.DictReader( csvfile, fieldnames)
 
 next(csvreader) # This skips the first row of the CSV file.
@@ -22,13 +22,13 @@ jsonfile.write('let world_vaccine_data = {"Sri Lanka": [\n')
 
 for row in csvreader:
     row_sum = {'t': row['Date'], 
-               'dose1': (int(row['Covishield 1']) + int(row['Sinopharm 1']) + int(row['Sputnik 1']) + int(row['Pfizer 1'])),
-               'dose2': (int(row['Covishield 2']) + int(row['Sinopharm 2']) + int(row['Sputnik 2']) + int(row['Pfizer 2']))
+               'dose1': (int(row['Covishield 1']) + int(row['Sinopharm 1']) + int(row['Sputnik 1']) + int(row['Pfizer 1']) + int(row['Moderna 1'])),
+               'dose2': (int(row['Covishield 2']) + int(row['Sinopharm 2']) + int(row['Sputnik 2']) + int(row['Pfizer 2']) + int(row['Moderna 2']))
               }
     json.dump(row_sum, jsonfile)
     jsonfile.write(',\n')
     
-jsonfile.write(']\n}')
+jsonfile.write(']\n}\n')
 
 if os.path.exists(csv_file_name):
   os.remove(csv_file_name)
